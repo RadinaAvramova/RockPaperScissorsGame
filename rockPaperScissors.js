446eec6055756439607c7bf2012cc676178561ff
RadinaAvramova <99686592+RadinaAvramova@users.noreply.github.com>
2023-01-22 21:28:44 +0200

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,37 +1,39 @@
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
+function rockPaperScissors (playerTurn) {
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissors = "Scissors";
 
-if (playerTurn == "r"  || playerTurn == "Rock") {
-    playerTurn = rock;
-} else if (playerTurn == "p"  || playerTurn == "Paper") {
-    playerTurn = paper;
-} else if (playerTurn == "s"  || playerTurn == "Scissors") {
-    playerTurn = scissors;
-} else {
-    console.log("Invalid Input. Try again...");
-}
+    if (playerTurn == "r"  || playerTurn == "Rock") {
+        playerTurn = rock;
+    } else if (playerTurn == "p"  || playerTurn == "Paper") {
+        playerTurn = paper;
+    } else if (playerTurn == "s"  || playerTurn == "Scissors") {
+        playerTurn = scissors;
+    } else {
+        console.log("Invalid Input. Try again...");
+    }
 
-let computerRandomNumber = Math.floor(Math.random() *3 ) + 1;
+    let computerRandomNumber = Math.floor(Math.random() *3 ) + 1;
 
-switch (computerRandomNumber) {
-    case 1:
-        computerMove = Rock;
-        break;
-    case 2:
-        omputerMove = Paper;
-        break;
-    case 1:
-        computerMove = Scissors;
-        break;
-}
+    switch (computerRandomNumber) {
+        case 1:
+            computerMove = "Rock";
+            break;
+        case 2:
+            omputerMove = "Paper";
+            break;
+        case 3:
+            computerMove = "Scissors";
+            break;
+    }       
 
-console.log(`The computer chooses ${computerTurn}`);
+    console.log(`The computer chooses ${computerTurn}`);
 
-if((playerTurn === rock && computerTurn === scissors) || (playerTurn === paper && computerTurn === rock) || (playerTurn === scissors && computerTurn === paper)) {
-    console.log("You win!");
-} else if () {
-    console.log("You lose!"); 
-} else {
-    console.log("This game was a draw");
+    if((playerTurn === rock && computerTurn === scissors) || (playerTurn === paper && computerTurn === rock) || (playerTurn === scissors && computerTurn === paper)) {
+        console.log("You win!");
+    } else if () {
+        console.log("You lose!"); 
+    } else {
+        console.log("This game was a draw");
+    }
 }
